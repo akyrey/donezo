dfe98422123e6129c852fc87827b1a8f764f905a
akyrey <stein.dario@gmail.com>
2026-03-24 23:17:29 +0100

    perf: add vite assets prefetching

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -30,6 +31,8 @@ final class AppServiceProvider extends ServiceProvider
     {
         Model::preventLazyLoading(!$this->app->isProduction());
         Model::preventSilentlyDiscardingAttributes(!$this->app->isProduction());
+
+        Vite::prefetch(concurrency: 3);
 
         if ($this->app->isLocal() && config('app.version') === '0.0.1-dev') {
             $version = mb_trim((string) shell_exec('git describe --tags --always 2>/dev/null'));
